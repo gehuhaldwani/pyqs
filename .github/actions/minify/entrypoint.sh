f311608653a55bcc1b132bf148c616509b574f4a
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2024-01-16 23:47:25 +0530

    possibly fixed

--- .github/actions/minify/entrypoint.sh
+++ .github/actions/minify/entrypoint.sh
@@ -3,8 +3,8 @@
 npm i minify -g
 apt-get update
 apt-get -y install moreutils
-${directory}
-find ${directory} -type f \( -iname \*.html -o -iname \*.js -o -iname \*.css \) | while read fname
+
+find $INPUT_DIRECTORY -type f \( -iname \*.html -o -iname \*.js -o -iname \*.css \) | while read fname
     do
     minify ${fname} | sponge ${fname}
     done
