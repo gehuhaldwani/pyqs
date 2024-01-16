098ee7896ca0d2425c58e93354307fbbc8b66ef4
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2024-01-16 23:22:30 +0530

    path fix

--- .github/actions/minify/entrypoint.sh
+++ .github/actions/minify/entrypoint.sh
@@ -4,6 +4,8 @@ npm i minify -g
 apt-get update
 apt-get -y install moreutils
 
+cd ${GITHUB_WORKSPACE}/_site
+
 find . -type f \( -iname \*.html -o -iname \*.js -o -iname \*.css \) | while read fname
     do
     minify ${fname} | sponge ${fname}
