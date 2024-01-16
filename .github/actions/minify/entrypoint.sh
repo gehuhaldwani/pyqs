0b847058612581dee5fb578022921a92dfdb9872
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2024-01-17 00:14:00 +0530

    maybe

--- .github/actions/minify/entrypoint.sh
+++ .github/actions/minify/entrypoint.sh
@@ -4,15 +4,12 @@ npm i minify -g
 apt-get update
 apt-get -y install moreutils
 
-cd ${INPUT_DIRECTORY}
-echo "Minifying HTML, JS, and CSS files in"
+cd _site
+echo "Minifying HTML, JS, and CSS files in "
 pwd
 
+
 find . -type f \( -iname \*.html -o -iname \*.js -o -iname \*.css \) | while read fname
-do
-  if minify ${fname} | sponge ${fname}; then
-    echo "Minified ${fname}"
-  else
-    echo "Error minifying ${fname}"
-  fi
-done
+    do
+    minify "${fname}" | sponge "${fname}"
+    done
