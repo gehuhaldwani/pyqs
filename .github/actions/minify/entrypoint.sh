e8986e72ef752a7660aec6ea22aaa6451367ea0d
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2024-01-16 23:52:21 +0530

    test4

--- .github/actions/minify/entrypoint.sh
+++ .github/actions/minify/entrypoint.sh
@@ -4,7 +4,9 @@ npm i minify -g
 apt-get update
 apt-get -y install moreutils
 
-find ./_site -type f \( -iname \*.html -o -iname \*.js -o -iname \*.css \) | while read fname
+echo "Minifying HTML, JS, and CSS files in ${GITHUB_WORKSPACE}\n"
+
+find ./ -type f \( -iname \*.html -o -iname \*.js -o -iname \*.css \) | while read fname
     do
     minify ${fname} | sponge ${fname}
     done
