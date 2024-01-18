82e338927c60e2b6a7c15ad5dbf6bdea68835368
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2024-01-18 21:38:42 +0530

    sudo it is

--- .github/workflows/minify.sh
+++ .github/workflows/minify.sh
@@ -9,15 +9,15 @@ pwd
 
 find . -type f \( -iname \*.html \) | while read fname; do
     echo "Minifying ${fname}"
-    prettydiff minify "${fname}" | sponge "${fname}"
+    prettydiff minify "${fname}" | sudo sponge "${fname}"
 done
 
 find . -type f \( -iname \*.js \) | while read fname; do
     echo "Minifying ${fname}"
-    uglifyjs "${fname}" | sponge "${fname}"
+    uglifyjs "${fname}" | sudo sponge "${fname}"
 done
 
 find . -type f \( -iname \*.css \) | while read fname; do
     echo "Minifying ${fname}"
-    lightningcss --minify --bundle --targets ">= 0.25%" "${fname}" | sponge "${fname}"
+    lightningcss --minify --bundle --targets ">= 0.25%" "${fname}" | sudo sponge "${fname}"
 done
