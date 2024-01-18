4c5b924ce1ec62d5d334738a0f245a84d470fa53
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2024-01-18 21:43:55 +0530

    minify parallization

--- .github/workflows/minify.sh
+++ .github/workflows/minify.sh
@@ -9,15 +9,18 @@ pwd
 
 find . -type f \( -iname \*.html \) | while read fname; do
     echo "Minifying ${fname}"
-    prettydiff minify "${fname}" | sudo sponge "${fname}"
+    prettydiff minify "${fname}" | sudo sponge "${fname}" &
 done
+wait
 
 find . -type f \( -iname \*.js \) | while read fname; do
     echo "Minifying ${fname}"
-    uglifyjs "${fname}" | sudo sponge "${fname}"
+    uglifyjs "${fname}" | sudo sponge "${fname}" &
 done
+wait
 
 find . -type f \( -iname \*.css \) | while read fname; do
     echo "Minifying ${fname}"
-    lightningcss --minify --bundle --targets ">= 0.25%" "${fname}" | sudo sponge "${fname}"
+    lightningcss --minify --bundle --targets ">= 0.25%" "${fname}" | sudo sponge "${fname}" &
 done
+wait
