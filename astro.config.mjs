dd18764b62f3ea9150a0d0379537511a6fa1379d
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2024-10-15 20:16:14 +0530

    baseurl and sitemap fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-    site: "http://localhost:4321",
+    site: "https://gehuhaldwani.github.io",
     base: '/pyqs',
     integrations: [mdx(), sitemap(), tailwind()],
     markdown: {
