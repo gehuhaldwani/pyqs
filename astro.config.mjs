4436314c45f1376366420e5e4a040b85dd954c4d
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2024-10-19 14:47:27 +0530

    updated configs and packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,10 @@
 import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://gehuhaldwani.github.io",
+    site: "https://gehuhaldwani.in",
     base: '/pyqs',
-    integrations: [mdx(), sitemap(), tailwind()],
-    markdown: {
-        shikiConfig: {
-          themes: {
-            light: 'poimandres',
-            dark: 'catppuccin-latte',
-          },
-        },
-      },
-    
+    integrations: [ sitemap(), tailwind()],
 })
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
+import typographyPlugin from "@tailwindcss/typography";
+
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	darkMode: "class",
 	theme: {
 		extend: {},
 	},
-	plugins: [require("@tailwindcss/typography")],
+	plugins: [typographyPlugin],
 };
