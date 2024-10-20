a0cd37bbe83554e6548f37308e8e1f06f6e25491
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2024-10-20 19:23:33 +0530

    paths, view tranistions (tags olny)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://gehuhaldwani.in",
-    base: '/pyqs',
-    integrations: [ sitemap(), tailwind()],
-})
+	site: "https://gehuhaldwani.in",
+	base: "/pyqs/",
+	trailingSlash: "always",
+	integrations: [sitemap(), tailwind()],
+});
--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,3 @@
 export const SITE_TITLE = "PYQ Archive";
-export const SITE_BASE = "/pyqs";
 export const SITE_DESCRIPTION =
 	"A collection of student contributed previous year question papers for Graphic Era Hill University semester examinations.";
