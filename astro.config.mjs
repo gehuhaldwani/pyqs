f47bb791db7083bb7108aea5c292f1d2a1c67f43
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2025-02-16 10:52:17 +0530

    package updates & tailwindcss v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://gehuhaldwani.in",
 	base: "/pyqs/",
 	trailingSlash: "always",
-	integrations: [sitemap(), tailwind(), mdx()],
+	integrations: [sitemap(), mdx()],
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
