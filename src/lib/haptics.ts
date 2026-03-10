fed147efd150e7adfcae2823e83904469f875939
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2026-03-10 17:51:07 +0530

    haptics fix

--- src/lib/haptics.ts
+++ src/lib/haptics.ts
@@ -1,6 +1,8 @@
 import { WebHaptics, defaultPatterns } from "web-haptics";
 
-const haptics = new WebHaptics();
+const haptics = new WebHaptics({
+    debug: import.meta.env.DEV,
+});
 
 export {
     haptics,
